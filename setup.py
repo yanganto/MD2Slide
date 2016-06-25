753bb8dca5a7b5bfe49a69dda13373e3d9ade933
poan_yang <poan_yang@trendmicro.com>
2016-06-25 20:21:15 +0800

    adding static files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="md2slide",
     packages=find_packages(),
-    version="0.0.1",
+    version="0.0.2",
     description="",
     author="yanganto",
     author_email="yanganto@gmail.com",
