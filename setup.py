bdc82342e615ff58c2b9d83852d99945a5348014
poan_yang <poan_yang@trendmicro.com>
2016-06-26 16:43:03 +0800

    set include_package_data True

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@ from setuptools import find_packages, setup
 setup(
     name="md2slide",
     packages=find_packages(),
-    version="0.0.2",
-    description="",
+    version="0.0.3",
+    description="A web server serve markdown file to html slide",
     author="yanganto",
     author_email="yanganto@gmail.com",
     url="https://github.com/yanganto/MD2Slide",
     download_url="",
     keywords=["markdown", "slide"],
+    license='MIT',
+    include_package_data=True,
+    package_data={
+        'md2slide': ['*.js', '*.css', '*.md'],
+    },
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
